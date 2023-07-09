99a3966c9c219daac1552708c2fe44bbf91422b7
Andrew-Sem <avvyayvayva@gmail.com>
2023-07-09 21:14:47 +0300

    base tRPC query for posts

---
+++ src/server/api/routers/posts.ts
@@ -0,0 +1,11 @@
+import {createTRPCRouter, publicProcedure} from "~/server/api/trpc";
+
+
+export const postRouter = createTRPCRouter({
+    getAll:publicProcedure.query(async ({ctx}) => {
+        return await ctx.prisma.post.findMany({
+            take: 100,
+            orderBy: [{createdAt: "desc"}]
+        })
+    })
+})
