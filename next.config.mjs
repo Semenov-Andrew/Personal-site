83cb88e405ca31fbc4c71a16b5ac10cb9161ac3f
Andrew-Sem <avvyayvayva@gmail.com>
2023-07-12 18:34:59 +0300

    setup contentlayer

--- next.config.mjs
+++ next.config.mjs
@@ -2,14 +2,16 @@
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
  * for Docker builds.
  */
-await import("./src/env.mjs")
+import { withContentlayer } from "next-contentlayer"
+
+import "./env.mjs"
 
 /** @type {import("next").NextConfig} */
 const config = {
-  reactStrictMode: true,
-  images: {
-    domains: ["images.clerk.dev"],
-  },
+    reactStrictMode: true,
+    images: {
+        domains: ["images.clerk.dev"],
+    },
 }
 
-export default config
+export default withContentlayer(config)
