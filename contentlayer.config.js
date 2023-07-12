7c63ed7f9755bb29fa35954d81f2900af3028da3
Andrew-Sem <avvyayvayva@gmail.com>
2023-07-12 20:00:21 +0300

    almost working contentlayer

--- contentlayer.config.js
+++ contentlayer.config.js
@@ -142,7 +142,7 @@ export const Page = defineDocumentType(() => ({
 }))
 
 export default makeSource({
-    contentDirPath: "./content",
+    contentDirPath: "./src/content",
     documentTypes: [Page, Doc, Guide, Post, Author],
     mdx: {
         remarkPlugins: [remarkGfm],
