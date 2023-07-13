43a322dcb4897696bb449a312ebce3adec8d77f1
Andrew-Sem <avvyayvayva@gmail.com>
2023-07-13 22:38:56 +0300

    add post authors

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,10 +1,11 @@
 import { type FC } from "react"
+import Image from "next/image"
 import Link from "next/link"
 import { notFound } from "next/navigation"
-import { allDocs } from "contentlayer/generated"
+import { allAuthors, allPosts } from "contentlayer/generated"
 
-import { cn } from "@/lib/utils"
-import { Button, buttonVariants } from "@/components/ui/Button"
+import { cn, formatDate } from "@/lib/utils"
+import { buttonVariants } from "@/components/ui/Button"
 import { Mdx } from "@/components/mdx-components"
 
 interface PageProps {
@@ -14,13 +15,16 @@ interface PageProps {
 }
 
 const getDocFromParams = (slug: string) => {
-    const doc = allDocs.find((doc) => doc.slugAsParams === slug)
-    if (!doc) notFound()
-    return doc
+    const post = allPosts.find((post) => post.slugAsParams === slug)
+    if (!post) notFound()
+    return post
 }
 
 const page: FC<PageProps> = ({ params }) => {
-    const doc = getDocFromParams(params.slug)
+    const post = getDocFromParams(params.slug)
+    const authors = post.authors.map((author) =>
+        allAuthors.find(({ slug }) => slug === `/authors/${author}`)
+    )
     return (
         <article className="container relative max-w-3xl py-6 lg:py-10">
             <Link
@@ -33,11 +37,48 @@ const page: FC<PageProps> = ({ params }) => {
                 See all posts
             </Link>
             <div>
+                {post.date && (
+                    <time
+                        dateTime={post.date}
+                        className="block text-sm text-muted-foreground"
+                    >
+                        Published on {formatDate(post.date)}
+                    </time>
+                )}
                 <h1 className="mt-2 inline-block font-heading text-4xl leading-tight lg:text-5xl">
-                    {doc.title}
+                    {post.title}
                 </h1>
+                {authors?.length ? (
+                    <div className="mt-4 flex space-x-4">
+                        {authors.map((author) =>
+                            author ? (
+                                <Link
+                                    key={author._id}
+                                    href={`https://github.com/${author.github}`}
+                                    className="flex items-center space-x-2 text-sm"
+                                >
+                                    <Image
+                                        src={author.avatar}
+                                        alt={author.title}
+                                        width={42}
+                                        height={42}
+                                        className="rounded-full bg-white "
+                                    />
+                                    <div className="flex-1 text-left leading-tight">
+                                        <p className="font-medium">
+                                            {author.title}
+                                        </p>
+                                        <p className="text-[12px] text-muted-foreground">
+                                            @{author.github}
+                                        </p>
+                                    </div>
+                                </Link>
+                            ) : null
+                        )}
+                    </div>
+                ) : null}
             </div>
-            <Mdx code={doc.body.code} />
+            <Mdx code={post.body.code} />
             <hr className="mt-12" />
             <div className="flex justify-center py-6 lg:py-10">
                 <Link
