cdd7010cd227339ceb9dafd8a47f60ed771094ae
Andrew Semyonov <101043571+Andrew-Sem@users.noreply.github.com>
2023-07-18 23:23:00 +0300

    add theme switcher, improve ui

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,11 +1,11 @@
 import { type FC } from "react"
-import Image from "next/image"
-import { images } from "@/constants/images"
+
+import { ThemeToggle } from "./theme-toggle"
 
 export const Footer: FC = () => {
     return (
-        <footer className="py-6">
-            <div className="container mx-auto flex justify-between">
+        <footer className="mt-10 py-6">
+            <div className="container mx-auto flex items-center justify-between">
                 <div className="text-sm">
                     <span className="text-muted-foreground">
                         &#64;&nbsp;{new Date().getFullYear()}&nbsp;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,7 @@ export default {
 
     theme: {
         container: {
+            center: true,
             padding: {
                 DEFAULT: "1rem",
             },
@@ -79,6 +80,24 @@ export default {
         backgroundImage: {
             "grid-pattern": "var(--grid-pattern)",
         },
+        keyframes: {
+            "accordion-down": {
+                from: { height: "0" },
+                to: { height: "var(--radix-accordion-content-height)" },
+            },
+            "accordion-up": {
+                from: { height: "var(--radix-accordion-content-height)" },
+                to: { height: "0" },
+            },
+        },
+        animation: {
+            "accordion-down": "accordion-down 0.2s ease-out",
+            "accordion-up": "accordion-up 0.2s ease-out",
+        },
     },
-    plugins: [require("@tailwindcss/container-queries")],
+    plugins: [
+        require("@tailwindcss/container-queries"),
+        require("tailwindcss-animate"),
+        require("@tailwindcss/typography"),
+    ],
 } satisfies Config
