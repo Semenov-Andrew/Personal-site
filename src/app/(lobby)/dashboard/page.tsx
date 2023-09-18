20033a333c3747a7cd6adde4336331e4009ac2d3
Andrew Semyonov <101043571+Andrew-Sem@users.noreply.github.com>
2023-09-18 23:43:52 +0300

    add files dropzone

--- src/app/(lobby)/dashboard/page.tsx
+++ src/app/(lobby)/dashboard/page.tsx
@@ -1,33 +1,39 @@
 "use client"
 
-import { UploadButton } from "@/lib/uploadthing"
+import { UploadButton, UploadDropzone } from "@/lib/uploadthing"
 import { buttonVariants } from "@/components/ui/button"
 
 const DashboardPage = () => {
     return (
         <div>
-            <UploadButton
-                appearance={{
-                    button: buttonVariants({ variant: "default" }),
-                }}
-                content={{
-                    button({ ready, isUploading }) {
-                        if (isUploading) return <div>Uploading...</div>
-                        if (ready) return <div>Upload stuff</div>
-                        return "Getting ready..."
-                    },
-                }}
-                endpoint="imageUploader"
-                onClientUploadComplete={(res) => {
-                    // Do something with the response
-                    console.log("Files: ", res)
-                    alert("Upload Completed")
-                }}
-                onUploadError={(error: Error) => {
-                    // Do something with the error.
-                    alert(`ERROR! ${error.message}`)
-                }}
-            />
+            <h2>Memes</h2>
+            <div className="grid grid-cols-2">
+                <UploadDropzone
+                    appearance={{
+                        uploadIcon: "w-12 h-12",
+                        button: buttonVariants({ variant: "default" }),
+                        container: "border-dashed p-6 min-h-[300px]",
+                    }}
+                    content={{
+                        button({ ready, isUploading }) {
+                            if (isUploading) return <div>Uploading...</div>
+                            if (ready) return <div>Upload stuff</div>
+                            return "Getting ready..."
+                        },
+                    }}
+                    endpoint="imageUploader"
+                    onClientUploadComplete={(res) => {
+                        // Do something with the response
+                        console.log("Files: ", res)
+                        alert("Upload Completed")
+                    }}
+                    onUploadError={(error: Error) => {
+                        // Do something with the error.
+                        alert(`ERROR! ${error.message}`)
+                    }}
+                />
+                <div></div>
+            </div>
         </div>
     )
 }
