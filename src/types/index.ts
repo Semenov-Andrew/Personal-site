f8bb2eb9732ad3423e4f2ca817d83ed60f3e40a3
Andrew Semyonov <101043571+Andrew-Sem@users.noreply.github.com>
2023-10-26 13:06:38 +0300

    remove unused types

--- src/types/index.ts
+++
@@ -1,5 +0,0 @@
-import { type z } from "zod"
-
-import { type userPrivateMetadataSchema } from "@/lib/validations/auth"
-
-export type UserRole = z.infer<typeof userPrivateMetadataSchema.shape.role>
