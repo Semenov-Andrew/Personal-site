a0c86db8dff146c4f935e43d2bac562f6bb706c0
Andrew Semyonov <101043571+Andrew-Sem@users.noreply.github.com>
2023-10-28 22:55:40 +0300

    next remote patterns

--- next.config.mjs
+++ next.config.mjs
@@ -10,10 +10,19 @@ import "./src/env.mjs"
 const config = {
     reactStrictMode: true,
     images: {
-        domains: [
-            "utfs.io",
-            "lh3.googleusercontent.com",
-            "avatars.githubusercontent.com",
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "utfs.io",
+            },
+            {
+                protocol: "https",
+                hostname: "lh3.googleusercontent.com",
+            },
+            {
+                protocol: "https",
+                hostname: "avatars.githubusercontent.com",
+            },
         ],
     },
 }
