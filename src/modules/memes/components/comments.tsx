8afebe635d9f71b06474f86ee922bcf8e337825e
Andrew Semyonov <101043571+Andrew-Sem@users.noreply.github.com>
2023-10-29 14:38:31 +0300

    Comment component

--- src/modules/memes/components/comments.tsx
+++ src/modules/memes/components/comments.tsx
@@ -4,6 +4,7 @@ import { Spinner } from "@/components/ui/spinner"
 import { api } from "@/trpc/react"
 import dayjs from "dayjs"
 import relativeTime from "dayjs/plugin/relativeTime"
+import { Comment } from "./comment"
 
 dayjs.extend(relativeTime)
 dayjs.locale("ru")
@@ -23,28 +24,7 @@ export const Comments = ({ memeId }: { memeId: string }) => {
     return (
         <div className="space-y-4 py-4">
             {comments.map((comment) => (
-                <div className="flex" key={comment.id}>
-                    <div className="flex space-x-2">
-                        <div className="shrink-0">
-                            <Image
-                                className="rounded-full"
-                                src={comment.image}
-                                width={40}
-                                height={40}
-                                alt={`${comment.commentatorName} image`}
-                            />
-                        </div>
-                        <div className="text-sm">
-                            <div className="mb-1 font-semibold">
-                                {comment.commentatorName}
-                            </div>
-                            <div>{comment.text}</div>
-                            <div className=" mt-1 text-muted-foreground">
-                                {dayjs(comment.createdAt).fromNow()}
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                <Comment comment={comment} />
             ))}
         </div>
     )
